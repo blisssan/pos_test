2110a4038f1fdb55ee1459b64867d2f0c0291807
Santhosh Kumar <thinkofsan@gmail.com>
2021-02-12 11:51:29 +0530

    fix

--- server_socket.dart
+++ server_socket.dart
@@ -1,17 +1,30 @@
 import 'dart:io';
 
 void main() {
-  ServerSocket.bind(InternetAddress.ANY_IP_V4, 4567).then(
-          (ServerSocket server) {
-        server.listen(handleClient);
-      }
-  );
+  ServerSocket.bind(InternetAddress.anyIPv4, 4567).then((ServerSocket server) {
+    server.listen(handleClient);
+  });
 }
 
-void handleClient(Socket client){
+void handleClient(Socket client) {
   print('Connection from '
       '${client.remoteAddress.address}:${client.remotePort}');
 
   client.write("Hello from simple server!\n");
+  client.listen(dataHandler,
+      onError: errorHandler, onDone: doneHandler, cancelOnError: false);
   client.close();
+}
+
+void dataHandler(data) {
+  print(new String.fromCharCodes(data).trim());
+}
+
+void errorHandler(error, StackTrace trace) {
+  print(error);
+}
+
+void doneHandler() {
+  // socket?.destroy();
+  // exit(0);
 }
